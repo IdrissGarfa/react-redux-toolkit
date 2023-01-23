35b6c90ebaa56a58033e4e7354ed06fc7660e73c
Idriss Garfa <idrissgarfa@gmail.com>
2023-01-23 15:01:59 +0100

    ADD incremenetByAmmount and reset actions

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,10 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import {
+    increment,
+    decrement,
+    reset,
+    incrementByAmout
+} from './counterSlice';
+
+import { useState } from 'react';
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
-    
+
+    const [incrementAmmount, setIncrementAmmount] = useState(0);
+
+    const addValue = Number(incrementAmmount) || 0;
+
+    const resetAll = () => {
+        setIncrementAmmount(0);
+        dispatch(reset());
+    }
+
     return (
         <section>
             <p>{count}</p>
@@ -12,8 +28,17 @@ const Counter = () => {
                 <button onClick={() => dispatch(increment())}>+</button>
                 <button onClick={() => dispatch(decrement())}>-</button>
             </div>
+            <input
+                type="text"
+                value={incrementAmmount}
+                onChange={(e) => setIncrementAmmount(e.target.value)}
+            />
+            <div>
+                <button onClick={() => dispatch(incrementByAmout(addValue))}>Add Amount</button>
+                <button onClick={resetAll}>Reset</button>
+            </div>
         </section>
-  )
+    )
 }
 
 export default Counter 
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -9,14 +9,20 @@ export const counterSlice = createSlice({
     initialState,
     reducers: {
         increment: (state) => {
-            state.count += 1
+            state.count += 1;
         },
         decrement: (state) => {
-            state.count -= 1
+            state.count -= 1;
+        },
+        reset: (state) => {
+            state.count = 0;
+        },
+        incrementByAmout: (state, action) => {
+            state.count += action.payload;
         }
     }
 })
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, reset, incrementByAmout } = counterSlice.actions;
 
 export default counterSlice.reducer;
